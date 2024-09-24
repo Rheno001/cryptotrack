77d226e47543d252cc80b63c1f1ab3ca9ba767be
Rheno001 <renfredreuben@gmail.com>
2024-09-24 21:12:39 +0100

    form styling done

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -6,6 +6,11 @@ const home = () => {
     <div className='home'>
       <div className="hero">
         <h1>Largest <br /> Crypto Marketplace</h1>
+        <p>Welcome to the world's largest cryptocurrency market place. Sign Up to explore more.</p>
+        <form action="">
+          <input type="text" placeholder='Search crypto...' />
+          <button type='submit'>Search</button>
+        </form>
       </div>
     </div>
   )
