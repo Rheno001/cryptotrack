a8be1ef25ff7c025163fd08b0b70938ad77c48f9
Rheno001 <renfredreuben@gmail.com>
2024-09-26 13:06:50 +0100

    adding crypto image, price and ranking

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,10 +1,34 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CoinContext = createContext();
 
 const CoinContextProvider = (props)=>{
 
-const contextValue = {}
+    const [allCoin, setAllCoin] = useState([]);
+    const [currency, setCurrency] = useState({
+        name: "USD",
+        symbol: "$"
+    })
+
+    const fetchAllCoin = async ()=>{
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-dsayiWFQwdkuMf9J2GaADLPb'}
+          };
+          
+          fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
+            .then(response => response.json())
+            .then(response => setAllCoin(response))
+            .catch(err => console.error(err));
+    }
+
+    useEffect(()=>{
+        fetchAllCoin()
+    },[currency])
+
+    const contextValue = {
+        allCoin, currency, setCurrency
+    }
 
     return(
         <CoinContext.Provider value={contextValue}>
