dc393cfed4282393a348887cac36cb5271e2ad15
Rheno001 <renfredreuben@gmail.com>
2024-09-30 21:59:12 +0100

    working on else statement

--- src/pages/Coin/coin.jsx
+++ src/pages/Coin/coin.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './coin.css'
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext';
 
 const coin = () => {
-  return (
-    <div>
-      <h2>coin</h2>
-    </div>
-  )
+
+  const {coinId} = useParams();
+  const[coinData, setCoinData] = useState();
+  const {currency} = useContext(CoinContext);
+
+  const fetchCoinData = async ()=>{
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(response => response.json())
+      .then(response => setCoinData(response))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+    fetchCoinData();
+  },[currency])
+
+  if(coinData){
+    return (
+      <div className='coin'>
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        </div>
+        
+      </div>
+    )
+
+  }else{
+    return (
+      <div className='spinner'>
+        <div className="spin"></div>
+   
+      </div>
+    )
+
+  }
+
+ 
 }
 
 export default coin
