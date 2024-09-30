02fc289cc33fcd4fe0cee713cc9397d2e0d8cf68
Rheno001 <renfredreuben@gmail.com>
2024-09-30 08:07:43 +0100

    working on coin page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './home.css'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const home = () => {
 
@@ -53,7 +54,7 @@ const home = () => {
         </div>
         {
           displayCoin.slice(0,10).map((item, index)=>(
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -62,7 +63,7 @@ const home = () => {
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
               <p className={item.price_change_percentage_24h>0?"green":"red"}>{Math.floor(item.price_change_percentage_24h*100)/100}%</p>
               <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
       </div>
